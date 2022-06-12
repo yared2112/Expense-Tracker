803b2128cef5d9efef4c05b70e6ae7653a2eceeb
yared2112 <teklewold.yared21@gmail.com>
2022-06-12 05:21:34 +0300

    completed task

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import Header from './components/Header';
+import React from 'react';
+import { Header } from './components/Header';
 import { Balance } from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses'
-import TransactionList from './components/TransactionList';
-import { AddTransaction } from './components/AddTransaction'; 
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
 import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
 
 function App() {
   return (
     <GlobalProvider>
-      <Header /> 
-      <div className='container'>
+      <Header />
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
-        <AddTransaction /> 
-      </div>   
+        <AddTransaction />
+      </div>
     </GlobalProvider>
   );
 }
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,21 +1,17 @@
-import React, { useContext } from 'react'
-import { Transaction } from './Transaction'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
 
-import { GlobalContext } from '../context/GlobalState'
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-    const { transactions } = useContext(GlobalContext)
-
+  const { transactions } = useContext(GlobalContext);
 
   return (
-      <>
+    <>
       <h3>History</h3>
-      <ul className='list'>
-      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} /> ))}
-        
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
-      </>
+    </>
   )
 }
-
-export default TransactionList
