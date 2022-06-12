994dae41e111d31a1e6978883722bd8f46b3ddad
yared2112 <teklewold.yared21@gmail.com>
2022-06-12 05:17:23 +0300

    completed task

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,18 +1,33 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' + (p[0].split('')[0]=== '-' ? '-' : '') +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
 
 export const Balance = () => {
-  const { transactions } = useContext(GlobalContext)
-  
-  const amounts = transactions.map(transaction => transaction.amount)
-  console.log(amounts)
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0);
 
   return (
     <>
-    <h4>Your Balance</h4>
-    <h1>{total} ETB</h1>
+      <h4>Your Balance</h4>
+    <h1>{moneyFormatter(total)}</h1>
     </>
   )
 }
-
