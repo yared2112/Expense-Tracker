bd204e7f808c81889e24e5eaeb3af48c3a3400fd
yared2112 <teklewold.yared21@gmail.com>
2022-06-12 05:06:14 +0300

    completed task

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,6 +5,7 @@ export const Balance = () => {
   const { transactions } = useContext(GlobalContext)
   
   const amounts = transactions.map(transaction => transaction.amount)
+  console.log(amounts)
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
 
   return (
