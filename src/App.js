a673f4bd240b1d4c36ccb3a610ac439b19aa04ce
yared2112 <teklewold.yared21@gmail.com>
2022-06-12 03:01:42 +0300

    completed task

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses'
+function App() {
+  return (
+    <div>
+      <Header /> 
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses /> 
+      </div>   
+    </div>
+  );
+}
+
+export default App;
